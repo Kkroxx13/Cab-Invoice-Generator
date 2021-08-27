b39352afaadd0562367f1359be57ab2c318d0a3a
Karan Kanchan <karan.kanchan@morningstar.com>
2021-08-27 16:09:27 +0530

    TestCase:Given less time and distance return minimum fare

--- InvoiceServiceTest/UnitTest1.cs
+++ InvoiceServiceTest/UnitTest1.cs
@@ -12,12 +12,32 @@ namespace InvoiceServiceTest
         [Test]
         public void GivenDistanceAndTime_ShouldReturnTotalFare()
         {
+            //Arrange
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
             double distance = 10.0;
             int time = 5;
+
+            //Act
             double fare = invoiceGenerator.CalculateFare(distance, time);
             double expected = 105;
+
+            //Assert
             Assert.AreEqual(expected, fare);
+
+        }
+        [Test]
+        public void GivenLessDistanceAndTime_ShouldReturnMinFare()
+        {
+            //Arrange
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            double distance = 0.2;
+            int time = 1;
+            //Act
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 5;
+            //Assert
+            Assert.AreEqual(expected, fare);
+
         }
     }
 }
