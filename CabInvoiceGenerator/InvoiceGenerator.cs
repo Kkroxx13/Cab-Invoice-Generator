f39cf59ad98b3ebd0e1e71aae6b5dd0597f02523
Karan Kanchan <karan.kanchan@morningstar.com>
2021-08-27 15:39:18 +0530

    Given distance and tim should return total fare

--- CabInvoiceGenerator/InvoiceGenerator.cs
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace CabInvoiceGenerator
 {
-    class InvoiceGenerator
+    public class InvoiceGenerator
     {
+        private static readonly int costPerTime = 1;
+        private static readonly double minimumCostPerKilometer = 10;
+        private static readonly double minimumFare = 5;
+        public double CalculateFare(double distance,int time)
+        {
+            double totalFare = distance * minimumCostPerKilometer + time * costPerTime;
+            if (totalFare < minimumFare)
+            {
+                return minimumFare;
+            }
+            return totalFare;
+        }
     }
 }
